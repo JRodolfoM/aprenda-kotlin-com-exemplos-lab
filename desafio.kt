0c915059044e791e733552ddfa1cb318267ac8c7
JRodolfoM <90014387+JRodolfoM@users.noreply.github.com>
2022-11-01 22:02:14 -0300

    entrga do projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+data class Usuario(val nome: String, val idade: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val listaConteudoEducacionalAndroid = listOf<ConteudoEducacional>(
+        ConteudoEducacional("Java", 50, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("Kotlin", 60, Nivel.BASICO),
+        ConteudoEducacional("JETPACK", 40, Nivel.AVANÇADO),
+
+        )
+    val listaConteudoEducacionalBackEnd = listOf<ConteudoEducacional>(
+        ConteudoEducacional("Java", 50, Nivel.AVANÇADO),
+        ConteudoEducacional("springBoot", 60, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("SQL", 20, Nivel.AVANÇADO)
+
+    )
+
+    val android = Formacao("Android", listaConteudoEducacionalAndroid, Nivel.AVANÇADO)
+    val backAnd = Formacao("Back End", listaConteudoEducacionalBackEnd, Nivel.INTERMEDIARIO)
+
+    android.matricular(Usuario("Jose", 28))
+    android.matricular(Usuario("Joao", 26))
+    android.matricular(Usuario("Ana", 21))
+
+    backAnd.matricular(Usuario("Pedro", 27))
+    backAnd.matricular(Usuario("Joao", 26))
+    backAnd.matricular(Usuario("Maria", 21))
+
+    println("Curso ${android.nome}  com Nivel ${android.nivel}")
+    android.conteudos.forEach{
+        println("${it.nome} com duração ${it.duracao} minutos com nivel ${it.nivel}")
+    }
+    android.inscritos.forEach {
+        println("${it.nome} está matriculado no curso de ${android.nome}")
+    }
+    println("-----------------------------------------------------------------------------------")
+    println("Curso ${backAnd.nome}  com Nivel ${backAnd.nivel}")
+    backAnd.conteudos.forEach{
+        println("${it.nome} com duração ${it.duracao} minutos com nivel ${it.nivel}")
+    }
+    backAnd.inscritos.forEach {
+        println("${it.nome} está matriculado no curso de ${backAnd.nome}")
+    }
+
+
 }
